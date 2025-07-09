2fedcaecb3138901abe53b7b48de77ca16d674ac
Alireza Afshan <yesterdayslemon@yahoo.com>
2025-07-09 18:07:57 +0300

    minor changes

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,8 +1,11 @@
 import type { Route } from "./+types/home";
-import {Link} from "react-router";
+import { Link } from "react-router";
 
 export function meta({}: Route.MetaArgs) {
-  return [{ title: "alireza afshan" }, { name: "alireza afshan", content: "alireza afshan" }];
+  return [
+    { title: "alireza afshan" },
+    { name: "alireza afshan", content: "alireza afshan" },
+  ];
 }
 
 export default function Home() {
@@ -10,8 +13,7 @@ export default function Home() {
     <div className="container mx-auto">
       <div className="flex h-screen flex-col items-center justify-center">
         <div className="bg-primary w-1/2 rounded-lg p-4">
-          
-          <div className="flex flex-col h-1/3">
+          <div className="flex h-1/3 flex-col">
             <h1 className="text-4xl font-bold">Alireza Afshan</h1>
             <h2 className="text-2xl font-bold">
               Software / Dev Ops / Information Systems
@@ -22,38 +24,29 @@ export default function Home() {
 
           <div className="h-1/3">
             <p>
-              email me at{" "}
+              email{" "}
               <a
                 className="text-blue-500"
-                href="mailto:alirezaafshan@gmail.com"
+                href="mailto:alirezaafshan4@gmail.com"
               >
-                alirezaafshan@gmail.com
+                me
               </a>
             </p>
             <p>
               check out my projects{" "}
-              <Link
-                className="text-blue-500"
-                to="/projects"
-              >
+              <Link className="text-blue-500" to="/projects">
                 here
               </Link>
             </p>
             <p>
               or on{" "}
-              <Link
-                className="text-blue-500"
-                to="/projects"
-              >
+              <a className="text-blue-500" href="https://github.com/YesterdaysLemon" target="_blank">
                 GitHub
-              </Link>
+              </a>
             </p>
             <p>
               if you want my resume,{" "}
-              <Link
-                className="text-blue-500"
-                to="/resume"
-              >
+              <Link className="text-blue-500" to="/resume">
                 click here
               </Link>
             </p>
